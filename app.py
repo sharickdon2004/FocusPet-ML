0286161cf4113bf87b2840c81f621e07b08e8e5e
jdvargassanchez <vjuandavid777@gmail.com>
2026-05-16 23:31:01 -0500

    Dataset explanation and other information was added and linked succesfully

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -9,3 +11,10 @@ def home():
 @app.route('/problematic')
 def problematic():
     return render_template('problematic.html')
+
+@app.route('/dataset')
+def dataset_explicacion():
+    csv_path = os.path.join(os.path.dirname(__file__), 'Dataset_Beta_FocusPet_V2 - Dataset_Beta_FocusPet_V2.csv')
+    df = pd.read_csv(csv_path)
+    records = df.to_dict(orient='records')
+    return render_template('dataset.html', dataset=records)
